cac6a7a9d6262e7648df42006877f5dec64cbddc
Ivan Toplak <itoplak@napster.com>
2019-09-24 09:29:38 +0200

    Creating CreateUserActivity UI

--- app/src/main/java/hr/from/ivantoplak/smack/LoginActivity.kt
+++ app/src/main/java/hr/from/ivantoplak/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package hr.from.ivantoplak.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
