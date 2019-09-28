91a60e76d1e3a140725963c89d5ce8992819bc40
Ivan Toplak <itoplak@napster.com>
2019-09-28 12:27:35 +0200

    Implementing generate user avatar and generate avatar background color features.

--- app/src/main/java/hr/from/ivantoplak/smack/CreateUserActivity.kt
+++ app/src/main/java/hr/from/ivantoplak/smack/CreateUserActivity.kt
@@ -1,10 +1,17 @@
 package hr.from.ivantoplak.smack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import hr.from.ivantoplak.smack.utils.*
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = AVATAR_DEFAULT_DRAWABLE
+    var avatarColor = AVATAR_DEFAULT_BACKGROUND_COLOR
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +19,27 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val color = Random.nextBoolean()
+        val avatar = (AVATAR_MIN..AVATAR_MAX).random()
 
+        userAvatar =
+            if (color) "$AVATAR_DRAWABLE_PREFIX_LIGHT$avatar" else "$AVATAR_DRAWABLE_PREFIX_DARK$avatar"
+        val resourceId = resources.getIdentifier(userAvatar, RES_DRAWABLE, packageName)
+        createUserAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateBackgroundColorBtnClicked(view: View) {
+        val colorRange = (COLOR_MIN..COLOR_MAX)
+        val red = colorRange.random()
+        val green = colorRange.random()
+        val blue = colorRange.random()
 
+        createUserAvatarImageView.setBackgroundColor(Color.rgb(red, green, blue))
+        val savedRed = red.toDouble() / COLOR_MAX
+        val savedGreen = green.toDouble() / COLOR_MAX
+        val savedBlue = blue.toDouble() / COLOR_MAX
+
+        avatarColor = "[$savedRed, $savedGreen, $savedBlue, $COLOR_DEFAULT_ALPHA]"
     }
 
     fun createUserBtnClicked(view: View) {
---
+++ app/src/main/java/hr/from/ivantoplak/smack/utils/Constants.kt
@@ -0,0 +1,12 @@
+package hr.from.ivantoplak.smack.utils
+
+const val AVATAR_MIN = 0
+const val AVATAR_MAX = 27
+const val COLOR_MIN = 0
+const val COLOR_MAX = 255
+const val RES_DRAWABLE = "drawable"
+const val AVATAR_DEFAULT_DRAWABLE = "profileDefault"
+const val AVATAR_DEFAULT_BACKGROUND_COLOR = "[0.5, 0.5, 0.5, 1]"
+const val AVATAR_DRAWABLE_PREFIX_LIGHT = "light"
+const val AVATAR_DRAWABLE_PREFIX_DARK = "dark"
+const val COLOR_DEFAULT_ALPHA = "1"
